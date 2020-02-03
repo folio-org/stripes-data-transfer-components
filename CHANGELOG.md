1f1e293d8608e4a65aeb4930e4a578fa5a94a764
Yevhenii Maltsev <yevhenii_maltsev@epam.com>
2020-02-03 15:03:25 +0200

    UIDEXP-14: Add shared job components

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,4 @@
 
 ## 1.0.0 (IN PROGRESS)
 * Module is created. Refs UIDEXP-11.
+* Move components from ui-data-import to display jobs and jobs lists. UIDEXP-14.
---
+++ index.js
@@ -0,0 +1,10 @@
+export { default as EndOfItem } from './lib/EndOfItem';
+export {
+  JobsList,
+  Job,
+  jobExecutionPropTypes,
+  sortRunningJobs,
+} from './lib/Jobs';
+export { Progress } from './lib/Progress';
+export { default as Preloader } from './lib/Preloader';
+export { sortCollection } from './lib/utils';
