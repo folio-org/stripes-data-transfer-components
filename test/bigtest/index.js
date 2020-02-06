da4d56fc69f9bc4f8ce1a029afdab71ea01d03f6
Yevhenii Maltsev <50317804+YevheniiMaltsev@users.noreply.github.com>
2020-02-06 15:56:14 +0200

    UIDEXP-14: Add shared job components (#4)
    UIDEXP-14: Add shared job components

--- test/bigtest/index.js
+++ test/bigtest/index.js
@@ -2,6 +2,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 // require all test files matching 'lib/**/tests/*-test'
-const requireTest = require.context('../lib/', true, /(.*?)\/tests\/(.*?)-test/);
+const requireTest = require.context('../../lib/', true, /(.*?)\/tests\/(.*?)-test/);
 
 requireTest.keys().forEach(requireTest);
