8d0e12c9010c8958039451a2c7716422bf5bf0d2
Victor Soroka <40821852+VictorSoroka1@users.noreply.github.com>
2020-05-16 00:25:50 +0300

    Fix tests (#55)

--- interactors.js
+++ interactors.js
@@ -1,3 +1,9 @@
 export * from './lib/SearchForm/tests/interactor';
 export * from './lib/SearchAndSortPane/tests/SearchAndSortInteractor';
 export * from './lib/FullScreenForm/tests/interactor';
+
+export {
+  mount,
+  mountWithContext,
+  wait,
+} from './test/bigtest/helpers';
