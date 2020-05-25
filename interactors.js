edfa1d1062753be39a8f6da90a17ba77f6055adc
Yevhenii Maltsev <yevhenii_maltsev@epam.com>
2020-05-25 16:37:13 +0300

    UIDEXP-82: Export PreloaderInteractor

--- interactors.js
+++ interactors.js
@@ -1,6 +1,7 @@
 export * from './lib/SearchForm/tests/interactor';
 export * from './lib/SearchAndSortPane/tests/SearchAndSortInteractor';
 export * from './lib/FullScreenForm/tests/interactor';
+export { default as PreloaderInteractor } from './lib/Preloader/tests/interactor';
 
 export {
   mount,
