d6be692a0f0c2be11f4fb704e3c6a0ff503bbb8d
Yevhenii Maltsev <50317804+YevheniiMaltsev@users.noreply.github.com>
2020-06-09 17:13:57 +0300

    UIDEXP-38: Export ProgressInteractor and update job execution prop types (#63)

--- interactors.js
+++ interactors.js
@@ -2,6 +2,7 @@ export * from './lib/SearchForm/tests/interactor';
 export * from './lib/SearchAndSortPane/tests/SearchAndSortInteractor';
 export * from './lib/FullScreenForm/tests/interactor';
 export { default as PreloaderInteractor } from './lib/Preloader/tests/interactor';
+export { default as ProgressInteractor } from './lib/Progress/tests/interactor';
 
 export {
   mount,
