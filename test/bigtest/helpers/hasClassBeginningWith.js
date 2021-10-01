ca502ebc100284e10c008ce6943df28b30871992
Vadym Shchekotilin <vadym_shchekotilin@epam.com>
2021-10-01 12:21:37 +0300

    STDTC-47 Remove Bigtest tests

--- test/bigtest/helpers/hasClassBeginningWith.js
+++
@@ -1,7 +0,0 @@
-import { computed } from '@bigtest/interactor';
-
-export function hasClassBeginningWith(selector, className) {
-  return computed(function () {
-    return this.$(selector).className.includes(className);
-  });
-}
--- test/bigtest/helpers/wait.js
+++
@@ -1,5 +0,0 @@
-export async function wait(timeout = 1000) {
-  await new Promise(resolve => {
-    setTimeout(resolve, timeout);
-  });
-}
