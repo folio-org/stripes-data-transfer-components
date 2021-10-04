c89404e75a1c70f83dcae3361669804920acc9eb
Vadym Shchekotilin <vadym_shchekotilin@epam.com>
2021-10-04 15:09:16 +0300

    STDTC-47 remove __new-mock__

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,2 +1,6 @@
 export * from './stripesResourcesMocks';
 export * from './Harness';
+export * from './mountWithContext';
+export * from './hasClassBeginningWith';
+export * from './wait';
+export * from './getHookExecutionResult';
---
+++ test/helpers/mountWithContext.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Harness } from './index';
+import { getCleanTestingRoot } from './getCleanTestingRoot';
+
+export function mountWithContext(component, translations = [], stripes) {
+  return new Promise(resolve => {
+    ReactDOM.render(
+      <Harness
+        translations={translations}
+        shouldMockOffsetSize={false}
+        stripes={stripes}
+      >
+        {component}
+      </Harness>,
+      getCleanTestingRoot(),
+      resolve
+    );
+  });
+}
