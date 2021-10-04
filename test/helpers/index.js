3f1b265d81c4470621493d5310021a01e5cf8f81
Vadym Shchekotilin <vadym_shchekotilin@epam.com>
2021-10-04 15:21:02 +0300

    STDTC-47 fix eslint problems

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,6 +1,4 @@
 export * from './stripesResourcesMocks';
 export * from './Harness';
-export * from './mountWithContext';
 export * from './hasClassBeginningWith';
 export * from './wait';
-export * from './getHookExecutionResult';
